b1cc18299d27ddb434de14868ff69c9c6e1152cf
AnonymousGrizzly <69007719+AnonymousGrizzly@users.noreply.github.com>
2022-01-10 18:36:35 +0100

    added a chartjs2 graph

--- grizzlycave/src/pages/Graphs.js
+++ grizzlycave/src/pages/Graphs.js
@@ -1,11 +1,42 @@
-import React from 'react'
+import React from 'react';
+import {Line, Pie} from 'react-chartjs-2';
 
-function Graphs() {
+export default function Graphs() {
+    const dataAct = {
+        labels: ["Jan", "Feb", "Mar", "Apr", "May", "Jun"],
+        datasets: [
+            {
+                label: "number of stored files",
+                fill: true,
+                backgroundColor: "rgba(75,192,192,0.2)",
+                borderColor: "rgba(75,192,192,1)"
+            }
+        ]
+    }
+    const dataToF = {
+        responsive: true,
+        labels: [],
+        datasets:[
+            {
+                
+                data:[]
+            }
+        ]
+    }
+
     return (
         <div className="graph-cntnr">
+            <div id="ActivitiesGraph">
+                <h2>Activities Chart</h2>
+                <p>- Take a look at when you were the most/least productive -</p>
+                <Line data={dataAct}/>
+            </div>
+            <div id="TypeOfFilesGraph">
+                <h2>Type Of Files Chart</h2>
+                <p>- Take a look at what kind of files you stored -</p>
+                <Pie data={dataToF}/>
+            </div>
             
         </div>
     )
 }
-
-export default Graphs
