2a40c106266cc994e9af3d7ee08f581e708dde6c
AnonymousGrizzly <69007719+AnonymousGrizzly@users.noreply.github.com>
2022-01-17 17:51:31 +0100

    put out canvas and graphs

--- grizzlycave/src/pages/SignUp.js
+++ grizzlycave/src/pages/SignUp.js
@@ -6,6 +6,8 @@ function SignUp() {
     //sam za sprobavanje
     const valid=true;
     const isSignedIn = false;
+    //konec sam za sprobavanje (kle se začne ta prav shit)
+
 
     function validateEmail(signup_email){ //Must contain @, can't be shorter than 8 characters
         const re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
@@ -18,8 +20,8 @@ function SignUp() {
     const signup_email = document.getElementById("signup_email"), signup_password = document.getElementById("signup_password"), signup_repassword = document.getElementById("repassword");
     //const valid = !!(validateEmail(signup_email) && validatePassword(signup_password) && signup_password == signup_repassword);
     let errormsg="0", emptymsg=!!(errormsg!=="0"); 
-    
-    if(!valid){
+     
+    if(!valid){  //to ne zgleda da bi delal
         if(!validatePassword(signup_password)){
             errormsg = "Password must contain at least 8 characters, 1 number, 1 letter & 1 unique character (!#$%/?...)";
         }else if(!validateEmail(signup_email)){
@@ -35,6 +37,7 @@ function SignUp() {
             <div className="title-cntnr">
                 <h1> You are about to enter <br/><b>GrizzlyCave</b> </h1><br/>
                 <p>- please provide the necessary information to get inside -</p>
+                <p>Toggle:</p><button onclick="">SignUp</button>
             </div>  
             {!isSignedIn && (
                 <form className="signup-cntnr">
