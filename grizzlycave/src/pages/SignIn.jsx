e60045914e6a13a963f2c58d8526e692db945607
AnonymousGrizzly <maks.rogelj.123@gmail.com>
2022-02-03 12:59:42 +0100

    signin

--- grizzlycave/src/pages/SignIn.jsx
+++ grizzlycave/src/pages/SignIn.jsx
@@ -6,25 +6,23 @@ import { AuthService } from '../services/auth';
 import {HashLink as Link} from 'react-router-hash-link';
 
 function SignIn() {
-  const 
-
+  const [username, setUsername] = useState("");
+    const [email, setEmail] = useState(""); 
+    const [password, setPassword] = useState("");
+    const [errorMsg, setErrorMsg] = useState(""); 
+    const handleSubmit = async () => {}
   return (
     <div>
       <div className='title-cntnr'>
                 <h1> You are about to enter <br/><b>GrizzlyCave</b> </h1><br/>
                 <p>- please provide the necessary information to get inside -</p><br/>
-                <Link smooth to="/signin" className="secondary-btn" > Already have an account? Login <i class="fas fa-sign-in-alt"></i></Link>
+                <Link smooth to="/signup" className="secondary-btn" > Don't have an account? SignUp! <i class="fas fa-sign-in-alt"></i></Link>
             </div>
         <div className="form-cntnr">
             <Input type="text" value={username} setValue={setUsername} placeholder="Username" required/>
-            <br/><p className='spam'>Must be unique</p>
             <Input type="text" value={email} setValue={setEmail} placeholder="Email" required />
-            <br/><p className='spam'>Must be valid email</p>
             <Input type="password" value={password} setValue={setPassword} placeholder="Password" required />
-            <p className='spam'>Must be complex</p>
-            <Input type="password" value={repassword} setValue={setRepassword} placeholder="Retype Password" required />
-            <br/><p className='spam'>Must be same as password</p>
-            <br/>
+            <br/><br/>
             <Button text={"Create User"} onClick={handleSubmit} className="submit-btn" />
             
         </div>
