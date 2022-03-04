88a9c8a14a62ee533cfee62e3950566e8de542f3
AnonymousGrizzly <maks.rogelj.123@gmail.com>
2022-03-04 21:22:13 +0100

    new database

--- database/grizzlybase.sql
+++ database/grizzlybase.sql
@@ -1,16 +1,9 @@
---OLD DATABASE! EXPORT NEW DATABASE!!!
-
-
-
-
-
-
 -- phpMyAdmin SQL Dump
 -- version 5.1.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Feb 02, 2022 at 10:21 AM
+-- Generation Time: Mar 04, 2022 at 09:21 PM
 -- Server version: 10.4.22-MariaDB
 -- PHP Version: 7.4.27
 
@@ -37,11 +30,10 @@ SET time_zone = "+00:00";
 CREATE TABLE `files` (
   `file_id` int(11) NOT NULL,
   `filename` varchar(64) NOT NULL,
-  `sanitized_name` varchar(80) NOT NULL,
   `filesize` int(11) NOT NULL,
   `filetype` varchar(64) NOT NULL,
   `deleted` tinyint(1) NOT NULL DEFAULT 0,
-  `modified_at` datetime NOT NULL,
+  `modified_at` datetime NOT NULL DEFAULT current_timestamp(),
   `created_at` datetime NOT NULL DEFAULT current_timestamp(),
   `user_id` int(11) NOT NULL,
   `folder_id` int(11) NOT NULL
@@ -76,7 +68,7 @@ CREATE TABLE `packets` (
   `sender_id` int(11) NOT NULL,
   `edit_perm` tinyint(1) NOT NULL DEFAULT 1,
   `created_packet` datetime NOT NULL DEFAULT current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -92,6 +84,16 @@ CREATE TABLE `users` (
   `created` datetime NOT NULL DEFAULT current_timestamp(),
   `modified` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`user_id`, `username`, `email`, `password`, `created`, `modified`) VALUES
+(3, 'test1', 'test1@gmail.com', '$2y$10$gC8Ee7ox2M4sNBQ1GVCGl.aiKScBy.YJvNzxC7awDNwbwU8f6/OCu', '2022-02-02 21:51:26', '2022-02-02 20:51:26'),
+(6, 'AnonymousGrizzly', 'maks.rogelj.123@gmail.com', '$2y$10$QoOaHN/ml4RWIVGTLJrfPeElTAOQcMYfkOA.R9pq3oUwMDIcYOFzC', '2022-02-04 11:13:43', '2022-02-04 10:13:43'),
+(7, 'testing', 'test2@test2.com', '$2y$10$4lV1AJ.GEGVomXGsmGMhLes8PJm8HvyCE4K6QSY7leKBXsB9RK77m', '2022-02-04 15:29:02', '2022-02-04 14:29:02'),
+(8, 'AnonymousShrimp', 'maks.rogelj@gmail.com', '$2y$10$N3WQDEzow9hVSbbS9gGi7eNFbpjgsxAnj1OzTdPpBwaahhkcu8cVe', '2022-02-05 12:38:53', '2022-02-05 11:38:53');
 
 --
 -- Indexes for dumped tables
@@ -118,9 +120,9 @@ ALTER TABLE `folders`
 --
 ALTER TABLE `packets`
   ADD PRIMARY KEY (`packet_id`),
+  ADD KEY `file_id` (`file_id`),
   ADD KEY `receiver_id` (`receiver_id`),
-  ADD KEY `sender_id` (`sender_id`),
-  ADD KEY `file_id` (`file_id`);
+  ADD KEY `sender_id` (`sender_id`);
 
 --
 -- Indexes for table `users`
@@ -133,6 +135,12 @@ ALTER TABLE `users`
 --
 -- AUTO_INCREMENT for dumped tables
 --
+
+--
+-- AUTO_INCREMENT for table `files`
+--
+ALTER TABLE `files`
+  MODIFY `file_id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `folders`
@@ -150,7 +158,7 @@ ALTER TABLE `packets`
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- Constraints for dumped tables
@@ -174,9 +182,9 @@ ALTER TABLE `folders`
 -- Constraints for table `packets`
 --
 ALTER TABLE `packets`
-  ADD CONSTRAINT `packets_ibfk_1` FOREIGN KEY (`receiver_id`) REFERENCES `users` (`user_id`),
-  ADD CONSTRAINT `packets_ibfk_2` FOREIGN KEY (`sender_id`) REFERENCES `users` (`user_id`),
-  ADD CONSTRAINT `packets_ibfk_3` FOREIGN KEY (`file_id`) REFERENCES `files` (`file_id`);
+  ADD CONSTRAINT `packets_ibfk_1` FOREIGN KEY (`file_id`) REFERENCES `files` (`file_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `packets_ibfk_2` FOREIGN KEY (`receiver_id`) REFERENCES `users` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `packets_ibfk_3` FOREIGN KEY (`sender_id`) REFERENCES `users` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
