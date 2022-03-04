dfc5ebb79b3ad7f406bd8688db6f614d665b8c00
AnonymousGrizzly <maks.rogelj.123@gmail.com>
2022-03-04 21:39:58 +0100

    database export

--- database/grizzlybase.sql
+++ database/grizzlybase.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 04, 2022 at 09:21 PM
+-- Generation Time: Mar 04, 2022 at 09:38 PM
 -- Server version: 10.4.22-MariaDB
 -- PHP Version: 7.4.27
 
