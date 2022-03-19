7fee6e44ff2146980ff1e71f43b42aa09d08ffe1
AnonymousGrizzly <maks.rogelj.123@gmail.com>
2022-03-19 22:57:51 +0100

    some mail css improvements

--- grizzlycave/src/pages/Mail.jsx
+++ grizzlycave/src/pages/Mail.jsx
@@ -1,33 +1,48 @@
-import React from 'react';
+import React, { useState } from 'react';
 import "../designs/Mail.css";
 import Input from "../components/Input"
 import Button from '../components/Button';
 
 function Mail() {
+
+  const [NumOfMail, setNumOfMail] = useState(false);
+
   return <div>
     <div id="mail" className='mail-cntnr'>
       <h1>File Send</h1>
       <div className='mailbox-cntnr'>
         <h3>Mailbox</h3> 
+        {NumOfMail ? (<div>
+          tole je pa treba še narest
+        </div>) : (
+          <p> You have no mail </p>
+        )}
       </div>
       <div className='postaloffice-cntnr'>
-        <h3 className='icon'>New Packet</h3><br/>
-        <form>
+        <h3 className='icon'>New Packet</h3>
+        <div>
           <p>to:</p>
-          <Input required placeholder="Receiver username" type="text"/>
-          <p>message:</p>
-          <Input placeholder={"message"}/>
-          <p>choose file:</p>
-          <Button
-          text={"Choose"}
-          className={"choose-btn"}
-          
-          /> <br/> <br/>
-          <Button
-          text={"SEND"}
-          className={"secondary-btn"}
+          <Input required
+           placeholder="Receiver username"
+            type="text"
           />
-        </form>
+          <p>short message:</p>
+          <Input
+           placeholder={"message"}
+          />
+          <p>choose file:</p>
+          <div className='office-btns'>
+            <Button
+            text={"Choose file"}
+            className={"choose-btn"}
+            
+            /><br/>
+            <Button
+            text={"SEND"}
+            className={"secondary-btn"}
+            />
+          </div>
+        </div>
       </div>
     </div>
   </div>;
