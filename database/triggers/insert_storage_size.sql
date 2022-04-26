ec2dba8991e9b9ce63be4fa1e3bff001a9870cee
AnonymousGrizzly <69007719+AnonymousGrizzly@users.noreply.github.com>
2022-04-26 08:33:36 +0200

    storage size was 1 Mb, changed to100 Mb

--- database/triggers/insert_storage_size.sql
+++ database/triggers/insert_storage_size.sql
@@ -5,8 +5,11 @@ FOR EACH ROW
     BEGIN
         DECLARE storage_size bigint;
         SELECT SUM(filesize) INTO storage_size FROM files WHERE user_id = NEW.user_id;
-        IF (storage_size + NEW.filesize)  > 1000000 THEN
+        IF (storage_size + NEW.filesize)  > 100000000 THEN
         SIGNAL SQLSTATE '1642' SET MESSAGE_TEXT = 'Storage space used up.';
         END IF;
     END //
 DELIMITER ;
+
+DELIMITER //
+CREATE TRIGGER storage_
