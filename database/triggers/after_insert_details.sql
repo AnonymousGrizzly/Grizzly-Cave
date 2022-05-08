7b11b40d41d9cf6c7d0d0050b0fef25bb2fb04ed
AnonymousGrizzly <maks.rogelj.123@gmail.com>
2022-05-08 16:35:08 +0200

    limit 5 details per person

---
+++ database/triggers/after_insert_details.sql
@@ -0,0 +1,15 @@
+--NOT IN DB YET
+
+DELIMITER //
+CREATE TRIGGER after_insert_details
+AFTER INSERT ON details
+FOR EACH ROW
+    BEGIN
+        DECLARE x int;                   
+        DECLARE last_details int;
+        SELECT COUNT(details_id) FROM details WHERE user_id = NEW.user_id INTO x;
+        SELECT details_id FROM details WHERE user_id = NEW.user_id ORDER BY details_id DESC LIMIT 0, 1 INTO last_details;
+        IF x > 5 THEN 
+        DELETE FROM details WHERE details_id = last_details;
+    END //
+DELIMITER ;
